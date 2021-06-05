7b16403bedf2e293bcf7298278136d3162fda9a4
hz991 <877338010@qq.com>
2021-06-05 10:03:59 +0800

    ggg制造冲突

--- src/com/igeek/test/Test.java
+++ src/com/igeek/test/Test.java
@@ -4,5 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("测试git版本");
         System.out.println("测试gggcao操作");
+        System.out.println("制造冲突。。。。。。。。。。。。。。。。。。");
+        System.out.println("ggg主分支");
     }
 }
